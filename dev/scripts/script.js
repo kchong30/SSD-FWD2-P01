d0780e54bcc6dc07be44fa5e26fc4b097c1c3d0b
sara rahmani <sararahmani96@gmail.com>
2023-02-07 17:57:49 -0800

    move js code to script file

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -52,3 +52,59 @@ $(window).resize(function () {
 $(window).on('orientationchange', function () {
     $('.news-carousel-container').not('.slick-initialized').slick('resize');
 });
+$(document).ready(function () {
+    $(".slick-slideshow").slick({
+      dots: false,
+      infinite: true,
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      arrows: true,
+      nextArrow: ".arrow.next",
+      prevArrow: ".arrow.previous",
+
+      //  variableWidth: true,
+
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            infinite: true,
+          },
+        },
+        {
+          breakpoint: 800,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+          },
+        },
+        ,
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+          },
+        },
+        {
+          breakpoint: 450,
+          // setting the settings property
+          // to 'unslick' to destroy the slideshow
+          settings: "unslick",
+        },
+
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ],
+    });
+  });
+  $(window).resize(function () {
+    $(".slick-slideshow").not(".slick-initialized").slick("resize");
+  });
+
+  $(window).on("orientationchange", function () {
+    $(".slick-slideshow").not(".slick-initialized").slick("resize");
+  });
