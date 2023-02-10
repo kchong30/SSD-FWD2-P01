9fca47f00bd833a17242b6c6204934349bb2f22c
Kevin Chong <kchong30@my.bcit.ca>
2023-02-09 17:42:55 -0800

    utility card implemented

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -147,3 +147,20 @@ function hideModal() {
   modalBackdrop.classList.remove("show");
 
 };
+
+//Eriks utility section
+
+const topButton = document.getElementById('page_top_link');
+
+window.onscroll = function () {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    topButton.style.display = "block";
+  } else {
+    topButton.style.display = "none";
+  }
+};
+
+topButton.addEventListener('click', () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
